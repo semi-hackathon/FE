306e01471568952c8a8ffeaf35e5b3b797a2ca49
이감재 <leegamgam123@gmail.com>
2025-08-03 06:20:48 +0900

    검색창 수정, 사이드바 디테일 수정

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import AnimePageLayout from "../components/animation/AnimePageLayout";
 
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  return (
+    <Container>
+      <Header>
+        <Title>찜 목록</Title>
+      </Header>
+    </Container>
+  )
 };
 
 export default FavoritesPage;
+
+const Container = styled.div`
+  padding: 1.5rem;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0rem 1.25rem;
+`;
+
+const Title = styled.div`
+  font-size: 1.8rem;
+  font-weight: bold;
+  color: white;
+`;
